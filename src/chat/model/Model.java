575f23ade01a718096dd5ae7f5d9d52cf6e5dca4
Bryan Tracy <btracy337@gmail.com>
2018-10-16 09:35:26 -0600

    Actually put code in Github.

--- src/chat/model/Model.java
+++ src/chat/model/Model.java
@@ -1,25 +1,38 @@
 package chat.model;
+
 import java.util.ArrayList;
 
 public class Model
 
 {
 	private ArrayList<String> responseList;
-	
 
 	private String currentUser;
 
-
 	private String joke;
 
-
 	private String content;
-	
+
 	private ArrayList<String> spookyList;
-	
+
 	public Model()
 	{
-		
+		this.joke = "Why did the chicken cross the road? To get to the other side!!!!!!!!";
+		this.currentUser = "default user --- boring";
+		this.content = "Empty of all content but not null.";
+		this.responseList = new ArrayList<String>();
+		this.spookyList = new ArrayList<String>();
+		buildTheLists();
+	}
+	private void buildTheLists()
+	{
+		responseList.add("Hello! How are you?");
+		responseList.add("Yes");
+		responseList.add("No");
+		responseList.add("That ain't it chief.");
+		responseList.add("Maybe");
+		responseList.add("YASSSSSSSSSSSS!");
+		responseList.add("Goodbye-- No more talking.");
 	}
 	public Model(String currentUser, String joke, String content, ArrayList<String> spookyList, ArrayList<String> responseList)
 	{
@@ -29,50 +42,58 @@ public class Model
 		this.spookyList = spookyList;
 		this.responseList = responseList;
 	}
-	//public String askname()
-	//{
-		
-	//}
+	// public String askname()
+	// {
+
+	// }
 	public String getcurrentUser()
 	{
 		return currentUser;
 	}
+
 	public String getJoke()
 	{
 		return joke;
 	}
+
 	public String getContent()
 	{
 		return content;
 	}
+
 	public ArrayList<String> getspookyList()
 	{
 		return spookyList;
 	}
+
 	public ArrayList<String> getresponseList()
 	{
 		return responseList;
 	}
+
 	public void setcurrentUser(String currentUser)
 	{
 		this.currentUser = currentUser;
 	}
+
 	public void setJoke(String joke)
 	{
 		this.joke = joke;
 	}
+
 	public void setContent(String content)
 	{
 		this.content = content;
 	}
+
 	public void spookyList(ArrayList<String> spookyList)
 	{
 		this.spookyList = spookyList;
 	}
+
 	public void responseList(ArrayList<String> responseList)
 	{
 		this.responseList = responseList;
 	}
-	
 
 }
